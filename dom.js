0471b15037da6c277def5f2a5d19b29e65003f26
Anikhet Mulky <am9559@g.rit.edu>
2024-03-19 02:20:51 -0400

    some dom manipulation

--- dom.js
+++ dom.js
@@ -25,3 +25,90 @@ h1.textContent = 'I am a h1';
 document.body.appendChild(h1);
 document.body.insertBefore(h1, btn);
 // document.body.removeChild(h1);
+
+//Event handler, no event listener = events are ignored
+btn.addEventListener('dblclick', function() {
+    console.log('I was clicked');
+    h1.style.color = 'blue';
+})
+
+btn.addEventListener('click', function() {
+    console.log('I was clicked');
+    h1.style.color = 'red';
+})
+
+
+// *Event Object*
+
+
+
+// Hell task 1
+
+var c = document.querySelector('#btt')
+var p = document.querySelector('p')
+c.addEventListener('click', function() {
+    p.textContent = 'I am a paragraph'
+})
+
+
+//Hell task 2
+// Swap images
+
+var img1 = document.querySelector('#i1')
+var img2 = document.querySelector('#i2')
+var btn = document.querySelector('#swap')
+btn.addEventListener('click', function() {
+    var temp = img1.src
+    img1.src = img2.src
+    img2.src = temp
+}
+)
+
+// Hell task 3 
+
+// When dealing with forms, submit button refreshes the pages which causes the js to not work
+
+var form = document.querySelector('form')
+var in1 = document.querySelector('#in1')
+var in2= document.querySelector('#in2')
+//var in = document.querySelectorAll('input[type="text"]')
+
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    if(in1.value === '' || in2.value ===''){
+        alert('The values are blank')
+}
+}
+)
+
+
+// Hell task 4
+
+var btn= document.querySelector('#inter')
+
+
+btn.addEventListener('click', function(){
+
+
+})
+
+
+
+var data = [
+    {
+        name:"Anikhet",
+         src:"https://avatars.githubusercontent.com/u/51974582?v=4"
+    },
+    {
+        name:"Tehkina",
+         src:"https://avatars.githubusercontent.com/u/51974582?v=4"
+    },
+    {
+        name:"Prison",
+         src:"https://avatars.githubusercontent.com/u/51974582?v=4"
+    },
+    {
+        name:"Pennywise",
+         src:"https://avatars.githubusercontent.com/u/51974582?v=4"
+    },
+]
